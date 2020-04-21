fca96d7f76239c8a7328ae2935b16ebd86ab5506
Tran Phuc Tai <tptai@huewaco.com.vn>
2020-04-21 09:47:13 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { AppRegistry } from 'react-native';
+import App from './src/App';
+import { name as appName } from './app.json';
+export default class DemoRedux2 extends Component {
+    render() {
+        return (
+            <App />
+        );
+    }
+}
+AppRegistry.registerComponent(appName, () => App);
